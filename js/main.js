189ab39867c88c137f33a5668fe6b42797cacf09
vera <verapoles1@mail.ru>
2020-06-18 15:15:46 +0300

    new

---
+++ js/main.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+	$(".f2 form").on('submit', function() {
+	alert('Сообщение отправлено');
+});
+	 $(".owl-carousel").owlCarousel({items:1, loop: true});
+     $("#phone").mask("+7(999) 999-99-99");
+     $("#tel1").mask("+7(999) 999-99-99");
+     $(".window-choice").on("change", function() {
+     	var src1 = $(".window-choice:checked").next().find("img").attr("src");
+     	var src2 = $(".window-choice:checked").next().find("p").text();
+     	$(".calculate-block-src").attr("src", src1);
+     	$(".good").text(src2);
+     });
+});
